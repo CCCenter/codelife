1583a4a48c2529379932e98bc73460defda25315
sheep <44664352+CCCenter@users.noreply.github.com>
2020-09-21 14:29:19 +0800

    Update PhoneCode.java

--- codelife-member-provider8003/src/main/java/com/codelife/cloud/member/util/PhoneCode.java
+++ codelife-member-provider8003/src/main/java/com/codelife/cloud/member/util/PhoneCode.java
@@ -34,8 +34,8 @@ public class PhoneCode {
         final String product = "Dysmsapi";//短信API产品名称（短信产品名固定，无需修改）
         final String domain = "dysmsapi.aliyuncs.com";//短信API产品域名（接口地址固定，无需修改）
         //替换成你的AK
-        final String accessKeyId = "LTAI4G6RVJRcgeHnRGe7GU9A";//你的accessKeyId,参考本文档步骤2
-        final String accessKeySecret = "bunGAHAJbPdEb4LJXNAZF833Odgq29";//你的accessKeySecret，参考本文档步骤2
+        final String accessKeyId = "xxxxxxxx";//你的accessKeyId,参考本文档步骤2
+        final String accessKeySecret = "xxxxxxxx";//你的accessKeySecret，参考本文档步骤2
         //初始化ascClient,暂时不支持多region（请勿修改）
         IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId,
                 accessKeySecret);
@@ -54,7 +54,7 @@ public class PhoneCode {
         //必填:短信签名-可在短信控制台中找到
         request.setSignName("codelife");
         //必填:短信模板-可在短信控制台中找到，发送国际/港澳台消息时，请使用国际/港澳台短信模版
-        request.setTemplateCode("SMS_189760500");
+        request.setTemplateCode("xxxxx");
         //可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
         //友情提示:如果JSON中需要带换行符,请参照标准的JSON协议对换行符的要求,比如短信内容中包含\r\n的情况在JSON中需要表示成\\r\\n,否则会导致JSON在服务端解析失败
         request.setTemplateParam("{\"code\":\"" + code + "\"}");
